dd3646d9c31ee17649e6202dfb887179bd583425
tom <tom.block@brockmann-consult.de>
2013-03-06 13:14:53 +0100

    added tests derived from IDL program

--- operational/src/test/java/org/esa/beam/occci/bandshift/PureWaterTest.java
+++ operational/src/test/java/org/esa/beam/occci/bandshift/PureWaterTest.java
@@ -36,7 +36,7 @@ public class PureWaterTest {
 
         assertEquals(2250, pureWater.getAw().length);
         assertEquals(3.07, pureWater.getAw()[0], 1e-10);
-        assertEquals(7061.60, pureWater.getAw()[2249], 1e-10);
+        assertEquals(7061.6, pureWater.getAw()[2249], 1e-10);
 
         assertEquals(2250, pureWater.getBw().length);
         assertEquals(0.183171, pureWater.getBw()[0], 1e-10);
@@ -53,15 +53,18 @@ public class PureWaterTest {
 
         assertEquals(2250, pureWater.getLambda().length);
         assertEquals(200.0, pureWater.getLambda()[0], 1e-10);
+        assertEquals(619.0, pureWater.getLambda()[419], 1e-10);
         assertEquals(2449.0, pureWater.getLambda()[2249], 1e-10);
 
         assertEquals(2250, pureWater.getAw().length);
         assertEquals(3.07, pureWater.getAw()[0], 1e-10);
+        assertEquals(5473.85, pureWater.getAw()[1833], 1e-10);
         assertEquals(7061.60, pureWater.getAw()[2249], 1e-10);
 
         assertEquals(2250, pureWater.getBw().length);
         assertEquals(0.151, pureWater.getBw()[0], 1e-10);
-        assertEquals(3.00000E-06, pureWater.getBw()[2249], 1e-10);
+        assertEquals(0.0000715, pureWater.getBw()[978], 1e-10);
+        assertEquals(0.0000030, pureWater.getBw()[2249], 1e-10);
     }
 
     @Test
@@ -73,21 +76,35 @@ public class PureWaterTest {
 
         assertEquals(2250, pureWater.getLambda().length);
         assertEquals(200.0, pureWater.getLambda()[0], 1e-10);
+        assertEquals(1322.0, pureWater.getLambda()[1122], 1e-10);
         assertEquals(2449.0, pureWater.getLambda()[2249], 1e-10);
 
         assertNull(pureWater.getAw());
 
         assertEquals(2250, pureWater.getBw().length);
         assertEquals(0.183171, pureWater.getBw()[0], 1e-10);
-        assertEquals(3.74577e-06, pureWater.getBw()[2249], 1e-10);
+        assertEquals(0.0000157777, pureWater.getBw()[1518], 1e-10);
+        assertEquals(0.00000374577, pureWater.getBw()[2249], 1e-10);
     }
 
     @Test
     public void testGetSpectralDataPureWater() throws Exception {
-        PureWater pureWater = PureWater.read();
-        double[] spectralDataPureWater = pureWater.getSpectralDataPureWater(413.0);
-        assertNotNull(spectralDataPureWater);
-        double[] expected = {0.004581862727272728, 0.005735880909090909};
+        final PureWater pureWater = PureWater.read();
+
+        double[] spectralDataPureWater = pureWater.getSpectralDataPureWater(400.0);
+        double[] expected = {0.0067231090909, 0.0065833863636};
+        assertArrayEquals(expected, spectralDataPureWater, 1e-10);
+
+        spectralDataPureWater = pureWater.getSpectralDataPureWater(413.0);
+        expected = new double[]{0.004581862727272728, 0.005735880909090909};
+        assertArrayEquals(expected, spectralDataPureWater, 1e-10);
+
+        spectralDataPureWater = pureWater.getSpectralDataPureWater(508.0);
+        expected = new double[]{0.0298133454545, 0.0023787354545};
+        assertArrayEquals(expected, spectralDataPureWater, 1e-10);
+
+        spectralDataPureWater = pureWater.getSpectralDataPureWater(619.0);
+        expected = new double[]{0.2741684545455, 0.0010402954545};
         assertArrayEquals(expected, spectralDataPureWater, 1e-10);
     }
 
