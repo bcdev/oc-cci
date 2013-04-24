ed94adcb2e8d29f2f059fde963663dfc09e3f6b4
tom <tom.block@brockmann-consult.de>
2013-04-24 11:11:48 +0200

    made output features configurable

--- operational/src/main/java/org/esa/beam/occci/qaa/binning/QaaDescriptor.java
+++ operational/src/main/java/org/esa/beam/occci/qaa/binning/QaaDescriptor.java
@@ -8,6 +8,8 @@ import org.esa.beam.binning.VariableContext;
 import org.esa.beam.occci.qaa.SensorConfig;
 import org.esa.beam.occci.qaa.SensorConfigFactory;
 
+import java.util.ArrayList;
+
 public class QaaDescriptor implements PostProcessorDescriptor {
 
     @Override
@@ -22,19 +24,47 @@ public class QaaDescriptor implements PostProcessorDescriptor {
 
     @Override
     public PostProcessor createPostProcessor(VariableContext varCtx, PostProcessorConfig postProcessorConfig) {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        final QaaConfig config = (QaaConfig) postProcessorConfig;
+        final String[] outputFeatureNames = createOutputFeatureNames(config);
+        return new QaaPostProcessor(outputFeatureNames);
     }
 
     // package access for testing only tb 2013-04-23
-    String[] createOutputFeatureNames(QaaConfig config) {
+    static String[] createOutputFeatureNames(QaaConfig config) {
         final SensorConfig sensorConfig = SensorConfigFactory.get(config.getSensorName());
+        final ArrayList<String> featureNameList = new ArrayList<String>();
 
-        // @todo 1 tb/tb continue here
-//        final int[] a_pig_out_indices = config.getA_pig_out_indices();
-//        for (int i = 0; i < a_pig_out_indices.length; i++) {
-//            int a_pig_out_indice = a_pig_out_indices[i];
-//
-//        }
-        return new String[0];  //To change body of created methods use File | Settings | File Templates.
+        final double[] wavelengths = sensorConfig.getWavelengths();
+        final int[] a_pig_out_indices = config.getA_pig_out_indices();
+        for (int a_pig_out_index : a_pig_out_indices) {
+            featureNameList.add("a_pig_" + getWavelengthInt(wavelengths, a_pig_out_index));
+        }
+
+        final int[] a_total_out_indices = config.getA_total_out_indices();
+        for (int a_total_out_index : a_total_out_indices) {
+            featureNameList.add("a_total_" + getWavelengthInt(wavelengths, a_total_out_index));
+        }
+
+        final int[] a_ys_out_indices = config.getA_ys_out_indices();
+        for (int a_ys_out_index : a_ys_out_indices) {
+            featureNameList.add("a_ys_" + getWavelengthInt(wavelengths, a_ys_out_index));
+        }
+
+        final int[] bb_spm_out_indices = config.getBb_spm_out_indices();
+        for (int bb_spm_out_index : bb_spm_out_indices) {
+            featureNameList.add("bb_spm_" + getWavelengthInt(wavelengths, bb_spm_out_index));
+        }
+
+        return featureNameList.toArray(new String[featureNameList.size()]);
+    }
+
+    // package access for testing only tb 2013-04-24
+    static int getWavelengthInt(double[] wavelengths, int index) {
+        if (index < 0 || index >= wavelengths.length) {
+            throw new IllegalArgumentException("Illegal wavelength index: " + index);
+        }
+
+        final double wavelength = wavelengths[index];
+        return (int) Math.round(wavelength);
     }
 }
--- operational/src/test/java/org/esa/beam/occci/qaa/binning/QaaDescriptorTest.java
+++ operational/src/test/java/org/esa/beam/occci/qaa/binning/QaaDescriptorTest.java
@@ -47,11 +47,68 @@ public class QaaDescriptorTest {
         config.setA_ys_out_indices(new int[]{4, 5});
         config.setBb_spm_out_indices(new int[0]);
 
-        final String[] featureNames = descriptor.createOutputFeatureNames(config);
+        final String[] featureNames = QaaDescriptor.createOutputFeatureNames(config);
         assertNotNull(featureNames);
 
         final String[] expected = {"a_pig_413", "a_pig_443", "a_total_490", "a_total_510", "a_ys_560", "a_ys_665"};
-        // @todo 1 tb/tb continue here
-        //assertArrayEquals(expected, featureNames);
+        assertArrayEquals(expected, featureNames);
+    }
+
+    @Test
+    public void testCreateOutputFeatureNames_MODIS_no_a_pig() {
+        final QaaConfig config = new QaaConfig();
+        config.setSensorName("MODIS");
+        config.setA_pig_out_indices(new int[0]);
+        config.setA_total_out_indices(new int[]{2, 3});
+        config.setA_ys_out_indices(new int[]{4});
+        config.setBb_spm_out_indices(new int[]{3, 4, 5});
+
+        final String[] featureNames = QaaDescriptor.createOutputFeatureNames(config);
+        assertNotNull(featureNames);
+
+        final String[] expected = {"a_total_488", "a_total_531", "a_ys_547", "bb_spm_531", "bb_spm_547", "bb_spm_667"};
+        assertArrayEquals(expected, featureNames);
+    }
+
+    @Test
+    public void testCreateOutputFeatureNames_SEAWIFS_no_a_total() {
+        final QaaConfig config = new QaaConfig();
+        config.setSensorName("SEAWIFS");
+        config.setA_pig_out_indices(new int[]{0, 1, 5});
+        config.setA_total_out_indices(new int[0]);
+        config.setA_ys_out_indices(new int[]{4});
+        config.setBb_spm_out_indices(new int[]{3, 4, 5});
+
+        final String[] featureNames = QaaDescriptor.createOutputFeatureNames(config);
+        assertNotNull(featureNames);
+
+        final String[] expected = {"a_pig_412", "a_pig_443", "a_pig_667", "a_ys_555", "bb_spm_510", "bb_spm_555", "bb_spm_667"};
+        assertArrayEquals(expected, featureNames);
+    }
+
+    @Test
+    public void testGetWavelengthInt() {
+        double[] wavelengths = new double[]{12.9, 13.5, 14.3};
+
+        assertEquals(13, QaaDescriptor.getWavelengthInt(wavelengths, 0));
+        assertEquals(14, QaaDescriptor.getWavelengthInt(wavelengths, 1));
+        assertEquals(14, QaaDescriptor.getWavelengthInt(wavelengths, 2));
+    }
+
+    @Test
+    public void testGetWavelengthInt_errorCases() {
+        double[] wavelengths = new double[]{13.9, 14.5, 15.3};
+
+        try {
+            QaaDescriptor.getWavelengthInt(wavelengths, -1);
+            fail("IllegalArgumentException expected");
+        } catch (IllegalArgumentException expected) {
+        }
+
+        try {
+            QaaDescriptor.getWavelengthInt(wavelengths, 3);
+            fail("IllegalArgumentException expected");
+        } catch (IllegalArgumentException expected) {
+        }
     }
 }
