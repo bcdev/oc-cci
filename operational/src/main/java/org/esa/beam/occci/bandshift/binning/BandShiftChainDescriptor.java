cb7eb662635208b941352dc8734dcb9d85f0b280
Marco Zuehlke <Marco.Zuehlke@brockmann-consult.de>
2013-10-15 16:41:13 +0200

    finalize band-shifting for MODIS, fix IOP band wl

--- operational/src/main/java/org/esa/beam/occci/bandshift/binning/BandShiftChainDescriptor.java
+++ operational/src/main/java/org/esa/beam/occci/bandshift/binning/BandShiftChainDescriptor.java
@@ -8,6 +8,8 @@ import org.esa.beam.binning.support.VariableContextImpl;
 import org.esa.beam.framework.gpf.annotations.Parameter;
 import org.esa.beam.occci.bandshift.Sensor;
 import org.esa.beam.occci.qaa.QaaConstants;
+import org.esa.beam.occci.qaa.binning.QaaConfig;
+import org.esa.beam.occci.qaa.binning.QaaDescriptor;
 import org.esa.beam.occci.util.binning.ChainProcessor;
 import org.esa.beam.occci.util.binning.MarkSensorProcessor;
 import org.esa.beam.occci.util.binning.SumToMeanCellProcessor;
@@ -18,6 +20,7 @@ import org.esa.beam.occci.util.binning.SumToMeanCellProcessor;
 public class BandShiftChainDescriptor implements CellProcessorDescriptor {
 
     public static final String NAME = "BandShiftChain";
+    private static final int[] BS_OUTPUT_CENTER_WAVELENGTHS = {412, 443, 490, 510, 555, 670};
 
     public static class Config extends CellProcessorConfig {
         @Parameter(notNull = true,
@@ -46,14 +49,13 @@ public class BandShiftChainDescriptor implements CellProcessorDescriptor {
 
         if (QaaConstants.MERIS.equals(config.sensorName)) {
 
-            String[] bandNames = {"Rrs412_mean", "Rrs443_mean", "Rrs490_mean", "Rrs510_mean", "Rrs560_mean", "Rrs665_mean", "a_pig_413_mean", "a_ys_413_mean", "bb_spm_413_mean"};
-            int[] outputCenterWavelengths = {412, 443, 490, 510, 555, 670};
+            String[] bsInputFeatures = {"Rrs412_mean", "Rrs443_mean", "Rrs490_mean", "Rrs510_mean", "Rrs560_mean", "Rrs665_mean", "a_pig_443_mean", "a_ys_443_mean", "bb_spm_443_mean"};
 
             BandShiftDescriptor bandShiftDescriptor = new BandShiftDescriptor();
             BandShiftConfig bandShiftConfig = (BandShiftConfig) bandShiftDescriptor.createConfig();
             bandShiftConfig.setSensorName(Sensor.MERIS.name());
-            bandShiftConfig.setBandNames(bandNames);
-            bandShiftConfig.setOutputCenterWavelengths(outputCenterWavelengths);
+            bandShiftConfig.setBandNames(bsInputFeatures);
+            bandShiftConfig.setOutputCenterWavelengths(BS_OUTPUT_CENTER_WAVELENGTHS);
             CellProcessor bandshiftProcessor = bandShiftDescriptor.createCellProcessor(varCtx, bandShiftConfig);
 
             VariableContext variableContext = createVariableContext(bandshiftProcessor.getOutputFeatureNames());
@@ -61,7 +63,38 @@ public class BandShiftChainDescriptor implements CellProcessorDescriptor {
 
             return new ChainProcessor(bandshiftProcessor, markProcessor);
         } else if (QaaConstants.MODIS.equals(config.sensorName)) {
-            return null;
+
+            String[] sumFeatureNames = {"Rrs_412_sum", "Rrs_443_sum", "Rrs_488_sum", "Rrs_531_sum", "Rrs_547_sum", "Rrs_667_sum"};
+            String weightFeatureName = "weights";
+            CellProcessor sumToMeanProcessor = new SumToMeanCellProcessor(varCtx, weightFeatureName, sumFeatureNames);
+
+            VariableContext qaaVarCtx = createVariableContext(sumToMeanProcessor.getOutputFeatureNames());
+            String[] qaaInputFeatures = {"Rrs_412", "Rrs_443", "Rrs_488", "Rrs_531", "Rrs_547", "Rrs_667"};
+            QaaDescriptor qaaDescriptor = new QaaDescriptor();
+            QaaConfig qaaConfig = (QaaConfig) qaaDescriptor.createConfig();
+            qaaConfig.setSensorName(QaaConstants.MODIS);
+            qaaConfig.setBandNames(qaaInputFeatures);
+            qaaConfig.setATotalOutIndices(new int[0]);
+            qaaConfig.setBbSpmOutIndices(new int[]{1});
+            qaaConfig.setAPigOutIndices(new int[]{1});
+            qaaConfig.setAYsOutIndices(new int[]{1});
+            qaaConfig.setRrsOut(true);
+            CellProcessor qaaProcessor = qaaDescriptor.createCellProcessor(qaaVarCtx, qaaConfig);
+
+            VariableContext bandShiftVarCtx = createVariableContext(qaaProcessor.getOutputFeatureNames());
+            String[] bsInputFeatures = {"Rrs_412", "Rrs_443", "Rrs_488", "Rrs_531", "Rrs_547", "Rrs_667", "a_pig_443", "a_ys_443", "bb_spm_443"};
+
+            BandShiftDescriptor bandShiftDescriptor = new BandShiftDescriptor();
+            BandShiftConfig bandShiftConfig = (BandShiftConfig) bandShiftDescriptor.createConfig();
+            bandShiftConfig.setSensorName(Sensor.MODISA.name());
+            bandShiftConfig.setBandNames(bsInputFeatures);
+            bandShiftConfig.setOutputCenterWavelengths(BS_OUTPUT_CENTER_WAVELENGTHS);
+            CellProcessor bandshiftProcessor = bandShiftDescriptor.createCellProcessor(bandShiftVarCtx, bandShiftConfig);
+
+            VariableContext markVarCtx = createVariableContext(bandshiftProcessor.getOutputFeatureNames());
+            CellProcessor markProcessor = new MarkSensorProcessor(markVarCtx, 1);
+
+            return new ChainProcessor(sumToMeanProcessor, qaaProcessor, bandshiftProcessor, markProcessor);
         } else if (QaaConstants.SEAWIFS.equals(config.sensorName)) {
 
             String[] sumFeatureNames = {"Rrs_412_sum", "Rrs_443_sum", "Rrs_490_sum", "Rrs_510_sum", "Rrs_555_sum", "Rrs_670_sum"};
