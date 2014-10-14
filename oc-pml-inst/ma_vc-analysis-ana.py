e9967a12244449bc79138f6023128b72a9debe6a
Marco Zuehlke <Marco.Zuehlke@brockmann-consult.de>
2014-10-14 17:19:35 +0200

    new gains, multiple bandsets

--- oc-pml-inst/ma_vc-analysis-ana.py
+++ oc-pml-inst/ma_vc-analysis-ana.py
@@ -5,18 +5,25 @@ import sys
 from pmonitor import PMonitor
 from daemon import Daemon
 ################################################################################
-sensors = ['MODIS', 'SEAWIFS']
+sensors = ['SEAWIFS', 'MODIS']
 sensorStations = {
     'MODIS': ['MOBY'],
     'SEAWIFS': ['MOBY', 'BOUSSOL', 'AAOT'],
 }
 gainsWithoutVC = {
-    'MODIS': 'CALIB        1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0',
-    'SEAWIFS': 'CALIB        1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0',
+    'MODIS': 'CALIB        1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 ',
+    'SEAWIFS': 'CALIB      1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0',
 }
 gainsWithVC = {
-    'MODIS': 'CALIB        0.998 1.004 1. 1.002 1.003 1.002 1. 1. 0.993 1. 1. 1. 1. 1.',
-    'SEAWIFS': 'CALIB 0.956 0.96 0.968 0.975 0.997 0.993',
+    'MODIS':
+        { 'default' : 'CALIB   0.995 1.001 1. 1.000 1.001 1.000 1. 1. 0.992 1. 1. 1. 1. 1. '},
+    'SEAWIFS':
+        {
+            'bandset_1' : 'CALIB 1. 1.007 1.000 1.002 1.015 1. 1. 1. ',
+            'bandset_2' : 'CALIB 1. 1.006 0.998 1.000 1.011 0.982 1. 1. ',
+            'bandset_3' : 'CALIB 1.048 1.033 1.014 1.013 1. 1. 1. 1. ',
+            'bandset_4' : 'CALIB 0.997 0.992 0.990 0.994 1.010 0.989 1. 1. ',
+          }
 }
 
 pointData = {
@@ -52,20 +59,22 @@ class MyDeamon(Daemon):
                 handle = "ingest-point-data-" + sensor + "-" + station
                 pm.execute('ingest-point-data.sh', [localPointData], [calvalusPointData], logprefix=handle)
                 #======================================================
-                vc = 'withvc'
-                params = ['ma_vc-analysis-ana-' + sensor + '-\${station}-\${vc}.xml',
-                          'station', station,
-                          'vc', vc,
-                          'calib', '"'+gainsWithVC[sensor]+'"',
-                          'calvalusPointData', calvalusPointData,
-                          'output', '/calvalus/projects/vc-analysis/' + sensor + "-" + station + "-" + vc
-                ]
-                handle = 'vc-analysis-ana-' + sensor + "-" + station + "-" + vc
-                pm.execute('template-step.py', [calvalusPointData], [handle], parameters=params, logprefix=handle)
+                for vc in gainsWithVC[sensor].keys():
+                    params = ['ma_vc-analysis-ana-' + sensor + '-\${station}-\${vc}.xml',
+                              'station', station,
+                              'sensor', sensor,
+                              'vc', vc,
+                              'calib', '"'+gainsWithVC[sensor]+'"',
+                              'calvalusPointData', calvalusPointData,
+                              'output', '/calvalus/projects/vc-analysis/' + sensor + "-" + station + "-" + vc
+                    ]
+                    handle = 'vc-analysis-ana-' + sensor + "-" + station + "-" + vc
+                    pm.execute('template-step.py', [calvalusPointData], [handle], parameters=params, logprefix=handle)
                 #======================================================
                 vc = 'withoutvc'
                 params = ['ma_vc-analysis-ana-' + sensor + '-\${station}-\${vc}.xml',
                           'station', station,
+                          'sensor', sensor,
                           'vc', vc,
                           'calib', '"'+gainsWithoutVC[sensor]+'"',
                           'calvalusPointData', calvalusPointData,
